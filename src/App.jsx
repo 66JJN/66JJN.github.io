9ea1f27bbc7dbed1df5b17866992adc5143bfb4f
66JJNN <pyaksda@gmail.com>
2026-01-27 21:53:51 +0700

    เพิ่มเนื้อหา

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="container">
+
+      {/* PROFILE */}
+      <section className="section">
+        <h1 className="name">John JJ</h1>
+        <h2 className="role">Frontend Developer (Intern)</h2>
+
+        <p className="description">
+          นักศึกษาสาย IT ที่สนใจด้าน Frontend Development  
+          มีพื้นฐาน React, JavaScript, HTML และ CSS  
+          ชอบพัฒนาเว็บไซต์ที่ใช้งานจริงและเน้นประสบการณ์ผู้ใช้
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+        <p className="description">
+          กำลังมองหาที่ฝึกงานตำแหน่ง Frontend Developer (PDKM)
+        </p>
+      </section>
+
+    </div>
   )
 }
 
 export default App
+
